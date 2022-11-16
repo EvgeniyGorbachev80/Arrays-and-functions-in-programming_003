72d38c449c41a2be6bfa6e917d91c3f36e8664fd
Evgeniy <evgeniy.gorbachev80@gmail.com>
2022-11-16 22:47:30 +0300

    add Task 036

---
+++ Task_036/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// Например:
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.Clear();
+Console.WriteLine();
+
+Console.Write("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите минимальное число массива: ");
+int min = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальное число массива: ");
+int max = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+ElementOddPosition(numbers);
+
+int result = ElementOddPosition(numbers);
+Console.WriteLine($"Сумма элементов нечётных позиций = {result}");
+
+int ElementOddPosition(int[] numbers) // Функция нахождения элементов на нечетных позициях.
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            result += numbers[i];
+        }
+    }
+    return result;
+}
+
+void FillArrayRandomNumbers(int[] array) // Функция заполнения массива.
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random random = new Random();
+        array[i] = random.Next(min, max);
+    }
+}
+
+void PrintArray(int[] array) // Функция вывода массива на экран.
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
